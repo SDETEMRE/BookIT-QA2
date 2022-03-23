1d97ce38435bb0a56448fbbc4888426ed8b62eba
SDETEMRE <dincsoyemre@gmail.com>
2022-03-23 16:51:10 +0300

    first commit

--- src/test/java/com/bookit/step_definitions/ApiStepDefs.java
+++ src/test/java/com/bookit/step_definitions/ApiStepDefs.java
@@ -1,6 +1,85 @@
 package com.bookit.step_definitions;
+import com.bookit.pages.SelfPage;
+import com.bookit.utilities.BookItApiUtils;
+import com.bookit.utilities.ConfigurationReader;
+import com.bookit.utilities.DBUtils;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.Assert;
+
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
 
 public class ApiStepDefs {
 
-}
+    static String token;
+    static Response response;
+    static String emailGlobal;
 
+    @Given("I logged Bookit api using {string} and {string}")
+    public void i_logged_Bookit_api_using_and(String email, String password) {
+
+        token = BookItApiUtils.generateToken(email, password);
+        emailGlobal = email;
+        String passwordGlobal;
+
+    }
+
+    @When("I get the current user information from api")
+    public void i_get_the_current_user_information_from_api() {
+        //send get request to retrieve current user information
+        String url = ConfigurationReader.get("qa2api.uri") + "/api/users/me";
+
+        response = given().accept(ContentType.JSON)
+                .and()
+                .header("Authorization", token)
+                .when()
+                .get(url);
+
+    }
+
+    @Then("status code should be {int}")
+    public void status_code_should_be(int statusCode) {
+
+        Assert.assertEquals(statusCode, response.statusCode());
+
+    }
+
+    @Then("the information about current user from api and database should match")
+    public void the_information_about_current_user_from_api_and_database_should_match() {
+        //API -DB
+        //get information from database
+        String query = "select id,firstname,lastname,role\n" +
+                "from users\n" +
+                "where email ='" + emailGlobal + "';";
+
+        Map<String, Object> rowMap = DBUtils.getRowMap(query);
+        System.out.println("rowMap = " + rowMap);
+        long expectedId = (long) rowMap.get("id");
+        String expectedFirstName = (String) rowMap.get("firstname");
+        String expectedLastName = (String) rowMap.get("lastname");
+        String expectedRole = (String) rowMap.get("role");
+
+        //get information from api
+        JsonPath jsonPath = response.jsonPath();
+
+        long actualId = jsonPath.getLong("id");
+        String actualFirstName = jsonPath.getString("firstName");
+        String actualLastName = jsonPath.getString("lastName");
+        String actualRole = jsonPath.getString("role");
+
+        //compare API - DB
+        Assert.assertEquals(expectedId, actualId);
+        Assert.assertEquals(expectedFirstName, actualFirstName);
+        Assert.assertEquals(expectedLastName, actualLastName);
+        Assert.assertEquals(expectedRole, actualRole);
+
+    }
+
+}
--- src/test/resources/features/user.feature
+++ src/test/resources/features/user.feature
@@ -2,7 +2,13 @@
 Feature: User Verification
 
   Scenario: verify information about logged user
-    Given I logged Bookit api using "sbirdbj@fc2.com" and "asenorval"
+    Given I logged Bookit api using "jalabaster7f@drupal.org" and "terimapam"
     When I get the current user information from api
     Then status code should be 200
+
+  @wip
+  Scenario: verify information about logged user from api and database
+    Given I logged Bookit api using "jalabaster7f@drupal.org" and "terimapam"
+    When I get the current user information from api
+    Then the information about current user from api and database should match
 
